76a0153d55fe59f1076da291e3b2576ac205c10f
Block <blockingeniero@gmail.com>
2023-01-07 19:22:15 +0100

    intentando faucet

--- back-end/router-faucet.js
+++ back-end/router-faucet.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const router = express.Router()
+// const express = require("express")
+// const router = express.Router()
 
-module.exports =  router
+// module.exports =  router
 
-router.post("/requestFund", (req, res) => {
-    res.status(500).send("Not implemented ")
-})
+// router.post("/requestFund", (req, res) => {
+//     res.status(500).send("Not implemented ")
+// })
--- front-end/components/faucet/faucet.jsx
+++ front-end/components/faucet/faucet.jsx
@@ -1,13 +1,68 @@
-const Faucet = () => {
+// const Faucet = () => {
 
-    return (
-        <main>
-            <header className="dash-titlebar">
-                <h3>Faucet</h3>
-            </header>
-        </main>
+//     return (
+//         <main>
+//             <header className="dash-titlebar">
+//                 <h3>Faucet</h3>
+//             </header>
+//         </main>
 
-    )
+//     )
+// }
+
+// export default Faucet;
+import { useState } from 'react'
+import { useEffect } from 'react'
+// import './App.css'
+const {ethereum} = window
+
+
+function App() {
+  const [cuenta, setCuenta] = useState(null)
+  const [saldo, setSaldo] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+//   async function enviarGoer() {
+//     setIsLoading(true)
+//     const response = await fetch(`http://localhost:3000/enviar/${cuenta}`)
+//     if (response.status == "200") {
+//       const datos = await response.json();
+//       await buscarSaldo(cuenta)
+//       setIsLoading(false)
+//     }
+//   }
+
+  async function buscarSaldo(cuenta) {
+    const response = await fetch(`http://localhost:3000/balance/${cuenta}`)
+    if (response.status == "200") {
+      const datos = await response.json();
+      setSaldo(datos)
+    }
+  }
+
+  // esuEffect es lo que permite conectarnos con el metamask
+  useEffect(() => {
+    ethereum.request({ method: 'eth_requestAccounts' }).then(cuentas => {
+      setCuenta(cuentas[0])
+      buscarSaldo(cuentas[0])
+
+      ethereum.on("accountsChanged", (cuentas)=> {
+          setCuenta(cuentas[0])
+          buscarSaldo(cuentas[0])
+
+      })
+    });
+
+  }, [])
+  // parte de la interfax
+  return (
+    <div className="container"> 
+      <div>Cuenta {cuenta}</div>
+      <div>Saldo = {JSON.stringify(saldo)}</div>
+      {!isLoading && <button onClick={() => enviarGoer()} className= 'mt-3 btn btn-primary'>Enviar 0.001 Goerl</button>}
+      {isLoading && <div>Se esta realizando la tx</div>}
+    </div>
+  )
 }
 
-export default Faucet;
+export default App
