7665821bec56949bfd4c187505ef0331acb1c48e
Block <blockingeniero@gmail.com>
2023-01-07 17:58:34 +0100

    añadir balance

---
+++ back-end/balance.js
@@ -0,0 +1,8 @@
+const Web3 = require("web3")
+const web3 = new Web3("http://127.0.0.1:5173")
+
+web3.eth.getBalance("0x704765a908962e25626f2bea8cdf96c84dedaa0b")
+.then(console.log)
+
+web3.eth.getBalance("0x9041142ec77b2f07032493Bf5e870Ae1D065c6F4")
+.then(console.log)
