6bb3054dce84c314bb59a4854bfdecc0b1d7d300
Hector Castro <hector.castro@scopely.com>
2023-01-10 20:05:40 +0100

    changes 09_01

--- back-end/router-personal.js
+++ back-end/router-personal.js
@@ -6,11 +6,13 @@ const cors = require('cors')
 
 router.use(cors())
 
-// const web3 = new Web3("http://localhost:8545")
-const web3 = new Web3("https://mainnet.infura.io/v3/20b55e65c65649c686d7abc8e4853f0b")
+const web3 = new Web3("http://localhost:8646")
+//const web3 = new Web3("https://mainnet.infura.io/v3/20b55e65c65649c686d7abc8e4853f0b")
 
 router.get("/saldo/:cuenta", async (req, res) =>{
     const saldo = await web3.eth.getBalance(req.params.cuenta)
     // res.send({ saldo })
-    res.send({ saldo : web3.utils.fromWei(saldo, 'ether') })
+    const saldoEthers = web3.utils.fromWei(saldo, 'ether')
+    res.send({ saldoEthers })
+    // res.send({ saldo : web3.utils.fromWei(saldo, ‘ether’) })
 })
