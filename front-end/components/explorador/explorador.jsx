9a5b6087672dc903c878b37fec0d4962b2b7071c
Hector Castro <hector.castro@scopely.com>
2023-01-10 22:09:20 +0100

    router-faucet-checked_v2

--- front-end/components/explorador/explorador.jsx
+++ front-end/components/explorador/explorador.jsx
@@ -1,17 +1,18 @@
 import { Outlet, useNavigate } from "react-router-dom"
 import { useForm } from 'react-hook-form'
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Explorador = () => {
 
-    const {register, handleSubmit} = useForm()
+    const { register, handleSubmit } = useForm()
     const navegar = useNavigate()
     const introducirInfo = (datos) => {
-        if(datos.datosForm.length == 66) // Si la longitud es 66 navegamos a tx
+        if (datos.datosForm.length == 66) // Si la longitud es 66 navegamos a tx
             navegar(`tx/${datos.datosForm}`)
-        if(datos.datosForm.length == 42) // Si la longitud es 42 navegamos a saldo
+        if (datos.datosForm.length == 42) // Si la longitud es 42 navegamos a saldo
             navegar(`saldo/${datos.datosForm}`)
-        if(/^\d+\.?\d*$/.test(datos.datosForm)) //Si el dato es un número navegamos a bloque
+        if (/^\d+\.?\d*$/.test(datos.datosForm)) //Si el dato es un número navegamos a bloque
             navegar(`bloque/${datos.datosForm}`)
 
     }
@@ -19,34 +20,46 @@ const Explorador = () => {
     const [redes, setRedes] = useState([]);
     const [redActiva, setredActiva] = useState([]);
     const getRedes = async () => {
-        // const sitesFromBack = await axios.get('http://localhost:3000/network/listAll');
-        // console.log(sitesFromBack.data);
-        // setRedes(sitesFromBack.data);
-        setRedes([1,2,3]);
-    }    
-    useEffect( () => {
-        getRedes();
-    } ,[]);
+        const sitesFromBack = await axios.get('http://localhost:3000/network/listAll');
+        console.log(sitesFromBack.data);
+        let tempArr = [];
 
-    return( 
-    <main>
-        <header className="dash-titlebar">
-            <h3>Explorador de redes locales</h3>
-        </header>
+        sitesFromBack.data.forEach((i) => {
+            let tempObj = {};
+            tempObj = i.name.substring(i.name.indexOf("net") + 3, i.name.indexOf("nodo"))
+            tempArr.push(parseInt(tempObj)); 
+        })
+
+        let arr = tempArr.filter( (value, index, self) => {
+            return self.indexOf(value) === index
+        })
+        console.log(arr.sort());
         
+        setRedes(arr.sort());
+    }
+    useEffect(() => {
+        getRedes();
+    }, []);
+
+    return (
+        <main>
+            <header className="dash-titlebar">
+                <h3>Explorador de redes locales</h3>
+            </header>
+
             <form className='d-flex gap-1 my-2 mx-2' onSubmit={handleSubmit(introducirInfo)}>
-                <select style={{color:"#B7C46E"}} className='btn btn-icon-label btn-accent-2' onChange={(e) => setredActiva(e.target.value.slice(4))}>
+                <select style={{ color: "#B7C46E" }} className='btn btn-icon-label btn-accent-2' onChange={(e) => setredActiva(e.target.value.slice(4))}>
                     {
                         redes.map((numRed) => <option key={numRed}>Red {numRed}</option>)
                     }
                 </select>
                 <input className="mx-1" {...register('datosForm')} size={100} placeholder=" Introduzca hash, número de bloque o cuenta"></input>
-                <button style={{color:"#B7C46E"}} className='btn btn-icon-label btn-accent-2'>Buscar</button>
+                <button style={{ color: "#B7C46E" }} className='btn btn-icon-label btn-accent-2'>Buscar</button>
             </form>
             <div className="border my-3 p-2">
                 <Outlet></Outlet>
             </div>
-</main>
+        </main>
     )
 }
 
